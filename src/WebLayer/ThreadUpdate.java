8df31f51ca6ae6a2a2a75ac068d8a983c1a10e8f
NhatVu <minhnhatse.uit@gmail.com>
2015-11-19 19:42:33 +0700

    Revert "[Nhat] - sua loi bai bi xoa"
    This reverts commit 3a6bf5bed1904e88a1be9b52520aca7031065554.

--- src/WebLayer/ThreadUpdate.java
+++ src/WebLayer/ThreadUpdate.java
@@ -98,10 +98,6 @@ public class ThreadUpdate {
 
         synchronized (_art) {
             FacebookDTO fbData = null;
-            ArticleDTO tempt = _art.getArticleInformation(_artUp.getUrl());
-            if (tempt.getTitle().equals(_artUp) == false) {
-                return;
-            }
             try {
                 fbData = _art.getContentOfFacebook(_artUp.getUrl());
             } catch (IOException ex) {
